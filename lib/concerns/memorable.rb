3ebdad6483191f884690b6709b8c5e74b7ecbf51
Jayon Kim <jpkim921@gmail.com>
2018-03-04 21:02:45 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,8 @@
 module Memorable
 
+# Remember that the self keyword is omitted when defining class methods inside modules. The extend keyword is responsible for defining the method as a class method vs. an instance method (which would use the include keyword).
+
+
   def reset_all
     all.clear
   end
